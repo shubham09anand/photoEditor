710dee3d23139422756f3558ef4fc062c98949d7
shubham09anand <114101710+shubham09anand@users.noreply.github.com>
2023-10-22 10:56:54 +0530

    Add files via upload

---
+++ collageOption.js
@@ -0,0 +1,17 @@
+function collageOptions() {
+  document.write(
+    "<div class='w-full text-2xl sm:text-3xl md:text-4xl text-center mb-10'>Other Frame Set</div><div class='text-gray-600 body-font w-screen'><div class='container px-5 mx-auto'><div class='flex-wrap -m-4 flex place-content-center items-center p-5 bg-gray-400 w-fit mx-auto'><div class='w-20 m-2'><a href='index.html' class='block relative h-20 rounded overflow-hidden'><img alt='ecommerce' class='bg-cover object-center w-full h-full block' src='Assets/frame-1.png'></a></div><div class='w-20 m-2'><a href='collageStyle5.html' class='block relative h-20 rounded overflow-hidden'><img alt='ecommerce' class='bg-cover object-center w-full h-full block' src='Assets/frame-2.png'></a></div><div class='w-20 m-2'><a href='collageStyle2.html' class='block relative h-20 rounded overflow-hidden'><img alt='ecommerce' class='bg-cover object-center w-full h-full block' src='Assets/frame-3.png'></a></div><div class='w-20 m-2'><a href='collageStyle3.html' class='block relative h-20 rounded overflow-hidden'><img alt='ecommerce' class='bg-cover object-center w-full h-full block' src='Assets/frame-4.png'></a></div><div class='w-20 m-2'><a href='collageStyle4.html' class='block relative h-20 rounded overflow-hidden'><img alt='ecommerce' class='bg-cover object-center w-full h-full block' src='Assets/frame-5.png'></a></div></div></div></div>"
+  );
+}
+
+function footer() {
+  document.write(
+    "<footer class='text-gray-600 body-font mt-20'> <div class='container px-5 py- mx-auto flex items-center sm:flex-row flex-col'><a class='flex title-font font-medium items-center md:justify-start justify-center text-gray-900'><div><img src='Assets/Logo_edit.Magazin.png' alt='' class='w-28 h-28'></div><div class='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'><a class='text-gray-500'></a><a class='ml-3 text-gray-500'></a><a href='https://instagram.com/shubham09anand?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D' class='ml-3 text-gray-500'><svg fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' class='w-5 h-5' viewBox='0 0 24 24'><rect width='20' height='20' x='2' y='2' rx='5' ry='5'></rect><path d='M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01'></path></svg></a></div></div></footer>"
+  );
+}
+
+function header() {
+  document.write(
+    "<div class='overflow-hidden'><div class='antialiased'><div class='w-screen text-gray-700 bg-transparent'><div class=' flex flex-col max-w-full px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8'><div class='flex flex-row items-center justify-between w-screen'><img id='logo' draggable='false' class='rounded-3xl h-20 w-20 sm:h-28 sm:w-28 md:h-40 md:w-40 p-2 mx-auto' src='Assets/Logo_edit.Magazin.png' alt=''></div></div></div></div></div>"
+    );
+}
